0d9145f957f91d9a03bfe52c8824ced3ac8ee915
ilona <gogiashvili.ilona@gmail.com>
2024-04-11 11:22:11 -0400

    commit tree 5c3b17c9db1bdff4c599740105ed58504af40eec

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (400 < feet && feet <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (2000 < feet && feet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(trip) {
   // Write your code here!
+  switch (trip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
